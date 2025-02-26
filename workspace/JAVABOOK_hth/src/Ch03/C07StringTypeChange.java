741e7b7046974876c39022cefc7fcda041d15f8d
HTH-0 <xogud1793@naver.com>
2025-02-26 11:09:57 +0900

    0226

---
+++ workspace/JAVABOOK_hth/src/Ch03/C07StringTypeChange.java
@@ -0,0 +1,22 @@
+package Ch03;
+
+public class C07StringTypeChange {
+	public static void main(String[] args) {
+		
+		
+		// 문자열 + 문자열
+		
+		System.out.println("문자열1" + "문자열2");
+		System.out.println("문자열1" + "문자열2");
+		System.out.println("문자열1" + ',' + "문자열2");
+		System.out.println("문자열1" + 2);
+		System.out.println("문자열1" + ',' + '!');
+		System.out.println( ',' + '!' + "문자열1");
+		System.out.println("문자열1" + "");
+		
+		
+		
+		
+		
+	}
+}
