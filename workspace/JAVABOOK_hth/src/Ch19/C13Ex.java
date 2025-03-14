525372a0a0e33e18c46515d22d67c141d585e7f8
HTH-0 <xogud1793@naver.com>
2025-03-14 17:28:58 +0900

    0314

--- workspace/JAVABOOK_hth/src/Ch19/C13Ex.java
+++ workspace/JAVABOOK_hth/src/Ch19/C13Ex.java
@@ -7,324 +7,402 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class C13Ex {
+
 	public static void main(String[] args) throws IOException, InterruptedException {
-		
-		// URL, PARAM 지정
-		String url = "https://www.daegufood.go.kr/kor/api/tasty.html";
-		String mode = "json";
-		String addr = "중구";
-		
-		url += "?mode=" + mode + "&addr=" + addr; 
 
-		// Http Request 객체 생성
-		HttpRequest httpRequest = HttpRequest.newBuilder()
-											 .uri(URI.create(url))
-											 .GET()
-											 .build();
+		// https://api.openweathermap.org/data/2.5/weather?q=Daegu&units=metric&lang=kr&appid=e3ef06f5e824287e524c96c57275ee57
+		String url = "https://api.openweathermap.org/data/2.5/weather";
+		String q = "Daegu";	// 지역
+		String units = "metric";	// 미터법
+		String lang = "kr";	// 국가
+		String appid = "e3ef06f5e824287e524c96c57275ee57";
+
+		url += "?q=" + q + "&units=" + units + "&lang=" + lang + "&appid=" + appid;
+		System.out.println(url);
+
+		// hr 객체 생성
+		HttpRequest hr = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
+
+		// hr 요청/ 응답확인
+		HttpClient hc = HttpClient.newHttpClient();
+
+		HttpResponse<String> response = hc.send(hr, HttpResponse.BodyHandlers.ofString());
+
+		// 가공 처리
+		ObjectMapper om = new ObjectMapper();
+
+		// Class type
+		Root ro = om.readValue(response.body(), Root.class);
 		
-		// Http Request 요청 / 응답 확인
-		HttpClient httpClient = HttpClient.newHttpClient();
-		
-		HttpResponse<String> response =
-		httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-		
-//		System.out.println(response.body());
-		
-		// 가공 처리(Class Type vs JSON Type)
-		
-		ObjectMapper objectMapper = new ObjectMapper();
-		
-		// Json type
-//		JsonNode jsonNode = objectMapper.readTree(response.body());
-		
-//		System.out.println("Status : " + jsonNode.get("status"));
-//		System.out.println("total : " + jsonNode.get("total"));
-		
-//		JsonNode data_arr = jsonNode.get("data");
-//		System.out.println("data : " + data_arr);
-//		
-//		for(int i = 0; i < data_arr.size(); i++) {
-//			JsonNode el = data_arr.get(i);
-//			System.out.println(el.get("BZ_NM"));
-//		}
-		
-		// Class Type
-		
-		ResponseObject responseObject = objectMapper.readValue(response.body(), ResponseObject.class);
-		
-		System.out.println("Status : " + responseObject.getStatus());
-		System.out.println("Total : " + responseObject.getTotal());
-		System.out.println("Data : " + responseObject.getData().get(0));
-				
-		
-		
+		// 출력되는 값들
+		System.out.printf("도시: %s\n", ro.getName());
+		System.out.printf("국가: %s\n", ro.getSys().getCountry());
+		System.out.printf("날씨 상태: %s (%s)\n", ro.getWeather().get(0).getMain(), ro.getWeather().get(0).getDescription());
+		System.out.printf("현재 온도: %.2f°C (체감: %.2f°C)\n", ro.getMain().getTemp(), ro.getMain().getFeels_like());
+		System.out.printf("온도 범위: 최저 %.2f°C / 최고 %.2f°C\n", ro.getMain().getTemp_min(), ro.getMain().getTemp_max());
+		System.out.printf("기압: %d hPa, 습도: %d%%\n", ro.getMain().getPressure(), ro.getMain().getHumidity());
+
+		if (ro.getMain().getSea_level() != 0) {
+		    System.out.printf("해수면 기압: %d hPa\n", ro.getMain().getSea_level());
+		}
+		if (ro.getMain().getGrnd_level() != 0) {
+		    System.out.printf("지면 기압: %d hPa\n", ro.getMain().getGrnd_level());
+		}
+
+		System.out.printf("가시 거리: %,d m\n", ro.getVisibility());
+		System.out.printf("구름량: %d%%\n", ro.getClouds().getAll());
+		System.out.printf("바람 속도: %.2f m/s (방향: %d°)\n", ro.getWind().getSpeed(), ro.getWind().getDeg());
+
+		if (ro.getWind().getGust() != 0) {
+		    System.out.printf("돌풍 속도: %.2f m/s\n", ro.getWind().getGust());
+		}
+		// 좌표 정보 출력
+		System.out.printf("위도: %.2f, 경도: %.2f\n", ro.getCoord().getLat(), ro.getCoord().getLon());
+
+		System.out.printf("일출 시간: %d\n", ro.getSys().getSunrise());
+		System.out.printf("일몰 시간: %d\n", ro.getSys().getSunset());
 	}
 
-	// import com.fasterxml.jackson.databind.ObjectMapper; // version 2.11.1
-	// import com.fasterxml.jackson.annotation.JsonProperty; // version 2.11.1
-	/* ObjectMapper om = new ObjectMapper();
-	Root root = om.readValue(myJsonString, Root.class); */
-	private static class Data{
-	    public String cnt;
-	    @JsonProperty("OPENDATA_ID") 
-	    public String oPENDATA_ID;
-	    @JsonProperty("GNG_CS") 
-	    public String gNG_CS;
-	    @JsonProperty("FD_CS") 
-	    public String fD_CS;
-	    @JsonProperty("BZ_NM") 
-	    public String bZ_NM;
-	    @JsonProperty("TLNO") 
-	    public String tLNO;
-	    @JsonProperty("MBZ_HR") 
-	    public String mBZ_HR;
-	    @JsonProperty("SEAT_CNT") 
-	    public String sEAT_CNT;
-	    @JsonProperty("PKPL") 
-	    public String pKPL;
-	    @JsonProperty("HP") 
-	    public String hP;
-	    @JsonProperty("PSB_FRN") 
-	    public String pSB_FRN;
-	    @JsonProperty("BKN_YN") 
-	    public String bKN_YN;
-	    @JsonProperty("INFN_FCL") 
-	    public String iNFN_FCL;
-	    @JsonProperty("BRFT_YN") 
-	    public String bRFT_YN;
-	    @JsonProperty("DSSRT_YN") 
-	    public String dSSRT_YN;
-	    @JsonProperty("MNU") 
-	    public String mNU;
-	    @JsonProperty("SMPL_DESC") 
-	    public String sMPL_DESC;
-	    @JsonProperty("SBW") 
-	    public String sBW;
-	    @JsonProperty("BUS") 
-	    public String bUS;
-	    
-	    Data() {}
-
-		public Data(String cnt, String oPENDATA_ID, String gNG_CS, String fD_CS, String bZ_NM, String tLNO,
-				String mBZ_HR, String sEAT_CNT, String pKPL, String hP, String pSB_FRN, String bKN_YN, String iNFN_FCL,
-				String bRFT_YN, String dSSRT_YN, String mNU, String sMPL_DESC, String sBW, String bUS) {
+	private static class Clouds {
+		
+		
+		public Clouds(int all) {
 			super();
-			this.cnt = cnt;
-			this.oPENDATA_ID = oPENDATA_ID;
-			this.gNG_CS = gNG_CS;
-			this.fD_CS = fD_CS;
-			this.bZ_NM = bZ_NM;
-			this.tLNO = tLNO;
-			this.mBZ_HR = mBZ_HR;
-			this.sEAT_CNT = sEAT_CNT;
-			this.pKPL = pKPL;
-			this.hP = hP;
-			this.pSB_FRN = pSB_FRN;
-			this.bKN_YN = bKN_YN;
-			this.iNFN_FCL = iNFN_FCL;
-			this.bRFT_YN = bRFT_YN;
-			this.dSSRT_YN = dSSRT_YN;
-			this.mNU = mNU;
-			this.sMPL_DESC = sMPL_DESC;
-			this.sBW = sBW;
-			this.bUS = bUS;
+			this.all = all;
 		}
 
-		public String getCnt() {
-			return cnt;
+		public int all;
+
+		public int getAll() {
+			return all;
 		}
 
-		public void setCnt(String cnt) {
-			this.cnt = cnt;
-		}
-
-		public String getoPENDATA_ID() {
-			return oPENDATA_ID;
-		}
-
-		public void setoPENDATA_ID(String oPENDATA_ID) {
-			this.oPENDATA_ID = oPENDATA_ID;
-		}
-
-		public String getgNG_CS() {
-			return gNG_CS;
-		}
-
-		public void setgNG_CS(String gNG_CS) {
-			this.gNG_CS = gNG_CS;
-		}
-
-		public String getfD_CS() {
-			return fD_CS;
-		}
-
-		public void setfD_CS(String fD_CS) {
-			this.fD_CS = fD_CS;
-		}
-
-		public String getbZ_NM() {
-			return bZ_NM;
-		}
-
-		public void setbZ_NM(String bZ_NM) {
-			this.bZ_NM = bZ_NM;
-		}
-
-		public String gettLNO() {
-			return tLNO;
-		}
-
-		public void settLNO(String tLNO) {
-			this.tLNO = tLNO;
-		}
-
-		public String getmBZ_HR() {
-			return mBZ_HR;
-		}
-
-		public void setmBZ_HR(String mBZ_HR) {
-			this.mBZ_HR = mBZ_HR;
-		}
-
-		public String getsEAT_CNT() {
-			return sEAT_CNT;
-		}
-
-		public void setsEAT_CNT(String sEAT_CNT) {
-			this.sEAT_CNT = sEAT_CNT;
-		}
-
-		public String getpKPL() {
-			return pKPL;
-		}
-
-		public void setpKPL(String pKPL) {
-			this.pKPL = pKPL;
-		}
-
-		public String gethP() {
-			return hP;
-		}
-
-		public void sethP(String hP) {
-			this.hP = hP;
-		}
-
-		public String getpSB_FRN() {
-			return pSB_FRN;
-		}
-
-		public void setpSB_FRN(String pSB_FRN) {
-			this.pSB_FRN = pSB_FRN;
-		}
-
-		public String getbKN_YN() {
-			return bKN_YN;
-		}
-
-		public void setbKN_YN(String bKN_YN) {
-			this.bKN_YN = bKN_YN;
-		}
-
-		public String getiNFN_FCL() {
-			return iNFN_FCL;
-		}
-
-		public void setiNFN_FCL(String iNFN_FCL) {
-			this.iNFN_FCL = iNFN_FCL;
-		}
-
-		public String getbRFT_YN() {
-			return bRFT_YN;
-		}
-
-		public void setbRFT_YN(String bRFT_YN) {
-			this.bRFT_YN = bRFT_YN;
-		}
-
-		public String getdSSRT_YN() {
-			return dSSRT_YN;
-		}
-
-		public void setdSSRT_YN(String dSSRT_YN) {
-			this.dSSRT_YN = dSSRT_YN;
-		}
-
-		public String getmNU() {
-			return mNU;
-		}
-
-		public void setmNU(String mNU) {
-			this.mNU = mNU;
-		}
-
-		public String getsMPL_DESC() {
-			return sMPL_DESC;
-		}
-
-		public void setsMPL_DESC(String sMPL_DESC) {
-			this.sMPL_DESC = sMPL_DESC;
-		}
-
-		public String getsBW() {
-			return sBW;
-		}
-
-		public void setsBW(String sBW) {
-			this.sBW = sBW;
-		}
-
-		public String getbUS() {
-			return bUS;
-		}
-
-		public void setbUS(String bUS) {
-			this.bUS = bUS;
-		}
-
-		@Override
-		public String toString() {
-			return "Data [cnt=" + cnt + ", oPENDATA_ID=" + oPENDATA_ID + ", gNG_CS=" + gNG_CS + ", fD_CS=" + fD_CS
-					+ ", bZ_NM=" + bZ_NM + ", tLNO=" + tLNO + ", mBZ_HR=" + mBZ_HR + ", sEAT_CNT=" + sEAT_CNT
-					+ ", pKPL=" + pKPL + ", hP=" + hP + ", pSB_FRN=" + pSB_FRN + ", bKN_YN=" + bKN_YN + ", iNFN_FCL="
-					+ iNFN_FCL + ", bRFT_YN=" + bRFT_YN + ", dSSRT_YN=" + dSSRT_YN + ", mNU=" + mNU + ", sMPL_DESC="
-					+ sMPL_DESC + ", sBW=" + sBW + ", bUS=" + bUS + "]";
-		}
-	    
-	    
-	}
-
-	private static class ResponseObject{
-	    public String status;
-	    public String total;
-	    public ArrayList<Data> data;
-	    
-		
-		public String getStatus() {
-			return status;
-		}
-		public void setStatus(String status) {
-			this.status = status;
-		}
-		public String getTotal() {
-			return total;
-		}
-		public void setTotal(String total) {
-			this.total = total;
-		}
-		public ArrayList<Data> getData() {
-			return data;
-		}
-		public void setData(ArrayList<Data> data) {
-			this.data = data;
+		public void setAll(int all) {
+			this.all = all;
 		}
 	}
 
+	private static class Coord {
+		public double lon;
+		public double lat;
+
+		public double getLon() {
+			return lon;
+		}
+
+		public void setLon(double lon) {
+			this.lon = lon;
+		}
+
+		public double getLat() {
+			return lat;
+		}
+
+		public void setLat(double lat) {
+			this.lat = lat;
+		}
+	}
+
+	private static class Main {
+		public double temp;
+		public double feels_like;
+		public double temp_min;
+		public double temp_max;
+		public int pressure;
+		public int humidity;
+		public int sea_level;
+		public int grnd_level;
+
+		public double getTemp() {
+			return temp;
+		}
+
+		public void setTemp(double temp) {
+			this.temp = temp;
+		}
+
+		public double getFeels_like() {
+			return feels_like;
+		}
+
+		public void setFeels_like(double feels_like) {
+			this.feels_like = feels_like;
+		}
+
+		public double getTemp_min() {
+			return temp_min;
+		}
+
+		public void setTemp_min(double temp_min) {
+			this.temp_min = temp_min;
+		}
+
+		public double getTemp_max() {
+			return temp_max;
+		}
+
+		public void setTemp_max(double temp_max) {
+			this.temp_max = temp_max;
+		}
+
+		public int getPressure() {
+			return pressure;
+		}
+
+		public void setPressure(int pressure) {
+			this.pressure = pressure;
+		}
+
+		public int getHumidity() {
+			return humidity;
+		}
+
+		public void setHumidity(int humidity) {
+			this.humidity = humidity;
+		}
+
+		public int getSea_level() {
+			return sea_level;
+		}
+
+		public void setSea_level(int sea_level) {
+			this.sea_level = sea_level;
+		}
+
+		public int getGrnd_level() {
+			return grnd_level;
+		}
+
+		public void setGrnd_level(int grnd_level) {
+			this.grnd_level = grnd_level;
+		}
+
+	}
+
+	private static class Root {
+		public Coord coord;
+		public ArrayList<Weather> weather;
+		public String base;
+		public Main main;
+		public int visibility;
+		public Wind wind;
+		public Clouds clouds;
+		public int dt;
+		public Sys sys;
+		public int timezone;
+		public int id;
+		public String name;
+		public int cod;
+
+		public Coord getCoord() {
+			return coord;
+		}
+
+		public void setCoord(Coord coord) {
+			this.coord = coord;
+		}
+
+		public ArrayList<Weather> getWeather() {
+			return weather;
+		}
+
+		public void setWeather(ArrayList<Weather> weather) {
+			this.weather = weather;
+		}
+
+		public String getBase() {
+			return base;
+		}
+
+		public void setBase(String base) {
+			this.base = base;
+		}
+
+		public Main getMain() {
+			return main;
+		}
+
+		public void setMain(Main main) {
+			this.main = main;
+		}
+
+		public int getVisibility() {
+			return visibility;
+		}
+
+		public void setVisibility(int visibility) {
+			this.visibility = visibility;
+		}
+
+		public Wind getWind() {
+			return wind;
+		}
+
+		public void setWind(Wind wind) {
+			this.wind = wind;
+		}
+
+		public Clouds getClouds() {
+			return clouds;
+		}
+
+		public void setClouds(Clouds clouds) {
+			this.clouds = clouds;
+		}
+
+		public int getDt() {
+			return dt;
+		}
+
+		public void setDt(int dt) {
+			this.dt = dt;
+		}
+
+		public Sys getSys() {
+			return sys;
+		}
+
+		public void setSys(Sys sys) {
+			this.sys = sys;
+		}
+
+		public int getTimezone() {
+			return timezone;
+		}
+
+		public void setTimezone(int timezone) {
+			this.timezone = timezone;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getCod() {
+			return cod;
+		}
+
+		public void setCod(int cod) {
+			this.cod = cod;
+		}
+
+	}
+
+	private static class Sys {
+		public String country;
+		public int sunrise;
+		public int sunset;
+
+		public String getCountry() {
+			return country;
+		}
+
+		public void setCountry(String country) {
+			this.country = country;
+		}
+
+		public int getSunrise() {
+			return sunrise;
+		}
+
+		public void setSunrise(int sunrise) {
+			this.sunrise = sunrise;
+		}
+
+		public int getSunset() {
+			return sunset;
+		}
+
+		public void setSunset(int sunset) {
+			this.sunset = sunset;
+		}
+
+	}
+
+	private static class Weather {
+		public int id;
+		public String main;
+		public String description;
+		public String icon;
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getMain() {
+			return main;
+		}
+
+		public void setMain(String main) {
+			this.main = main;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public String getIcon() {
+			return icon;
+		}
+
+		public void setIcon(String icon) {
+			this.icon = icon;
+		}
+
+	}
+
+	private static class Wind {
+		public double speed;
+		public int deg;
+		public double gust;
+
+		public double getSpeed() {
+			return speed;
+		}
+
+		public void setSpeed(double speed) {
+			this.speed = speed;
+		}
+
+		public int getDeg() {
+			return deg;
+		}
+
+		public void setDeg(int deg) {
+			this.deg = deg;
+		}
+
+		public double getGust() {
+			return gust;
+		}
+
+		public void setGust(double gust) {
+			this.gust = gust;
+		}
+
+	}
 
 }
-
