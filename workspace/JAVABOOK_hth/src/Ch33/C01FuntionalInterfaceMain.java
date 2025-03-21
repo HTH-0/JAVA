cf16f34ea9f591b25afdeebd9e3fb9d505219ac3
HTH-0 <xogud1793@naver.com>
2025-03-21 11:20:40 +0900

    0321

--- workspace/JAVABOOK_hth/src/Ch33/C01FuntionalInterfaceMain.java
+++ workspace/JAVABOOK_hth/src/Ch33/C01FuntionalInterfaceMain.java
@@ -1,9 +1,24 @@
 package Ch33;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @FunctionalInterface // 함수형 인터페이스 선언(Compiler 에게 전달) >> 메서드 무조건 한개만!!
 interface Func1 {
 
 	void say(String message);
+}
+
+@FunctionalInterface
+interface Func2 {
+
+	int sum(int ...args);
+}
+
+@FunctionalInterface
+interface Func3 {
+	List<Integer> createdListDesc(int ... args);
 }
 
 public class C01FuntionalInterfaceMain {
@@ -15,9 +30,38 @@ public class C01FuntionalInterfaceMain {
 		Func1 f2 = System.out::println;
 		f2.say("HELLO WORLD2");
 
-		Func1 f3 = str -> {System.out.println(str + "...");};
-		f3.say("hi");
+		Func2 func2 = (arg) -> {
+			int sum = 0;
+			for(int el : arg) {
+				sum += el;
+			};
+			return sum;
+		};
+		// {} 에 모든 items 더한 값을 리턴 (반복문으로 items의 item을 누적해서 리턴
+		System.out.println(func2.sum(10, 20, 30, 40, 556, 6, 7, 8, 9));
+
 		
-	};
+		
+		Func2 func2_2 = (arg) -> {
+			// {} 에 모든 items를 더한 값을 리턴 (reduce)
+			
+			return Arrays.stream(arg)
+					.reduce(0,(sum, el) -> {return sum + el;});
+		};
+		System.out.println(func2_2.sum(10, 20, 30, 40, 556, 6, 7, 8, 9));
+	
+		
+		// 원시 타입 int[] 받기 >> 내림차순 >> int 형을 Integer 로 boxing >> List<Integer> 변환
+		Func3 func3 = (items) -> {
+			return Arrays.stream(items)
+			.boxed()
+			.sorted((a,b) -> {return b-a;})
+			.collect(Collectors.toList());
+			};
+		
+		List<Integer> li = func3.createdListDesc(10, 100, 24, 1612, 42, 123, 15, 4 ,1);
+		
+		System.out.println(li);
+	}
 
 }
---
+++ workspace/JAVABOOK_hth/src/Ch33/C02Ex.java
@@ -0,0 +1,64 @@
+package Ch33;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@FunctionalInterface
+interface Functional{
+	Integer execute(int ...args);
+}
+
+class Calc{
+	Functional sum;
+	Functional sub;
+	Functional mul;
+	Functional div;
+	
+	Calc(){
+		
+		// 람다식 완성
+		// 모든 인자를 받을 수 있는 가변 인자 처리로 구현
+		// 뺄셈, 나눗셈은 큰 수에서 작은 수로 정렬 >> 누적 감산 처리(reduce)
+		sum = (arg) -> {
+			int hap = 0;
+			for(int el : arg) {
+				hap += el;
+			}
+			return hap ;
+		};
+		sub = (arg)->{
+			return Arrays.stream(arg)
+					.boxed()
+					.sorted((a,b) -> {return b-a;})
+					.reduce((a,b)->{return a - b;})
+					.orElse(null);
+		};
+		
+		mul = (arg)->{
+			int gop = 1;
+			for(int el : arg) {
+				gop *= el;
+			}
+			return gop;
+		};
+		div = (arg)->{
+			return Arrays.stream(arg)
+					.boxed()
+					.sorted((a,b) -> {return b-a;})
+					.reduce((a,b)->{return a / b;})
+					.orElse(0);
+		};
+	}
+}
+
+
+public class C02Ex {
+	public static void main(String[] args) {
+		
+		Calc calc = new Calc();
+		System.out.println("합 : " + calc.sum.execute(10,20,30,40,50,60));
+		System.out.println("차 : " + calc.sub.execute(1, 4, 2, 16)); // 1, 2, 4, 16
+		System.out.println("곱 : " + calc.mul.execute(10,20,30,40,50,60));
+		System.out.println("나눗셈 : " + calc.div.execute(1, 4, 2, 16));
+	}
+}
